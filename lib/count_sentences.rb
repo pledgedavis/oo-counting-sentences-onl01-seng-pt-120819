54f02e95b951b830b2c11812c2db189b00ae59dc
pledgedavis <pledgedavis14@gmail.com>
2020-01-10 20:33:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,7 +18,7 @@ self.end_with?("!")
   def count_sentences
     binding.pry
     self.split.count
-    self.split(/\.|\!|\?/)
+    self.split(/\.+|\!+|\?+/)
     # Do x
   # Do y
 # self.count("") 
